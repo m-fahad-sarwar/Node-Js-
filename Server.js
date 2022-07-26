db9c2816d726c33e80847f9b1d82334998a13302
M Fahad Sarwar <sarwarf271@gmail.com>
2022-07-26 13:48:24 +0500

    changes

--- Server.js
+++ Server.js
@@ -1,20 +1,35 @@
-const express = require("express");
-var bodyParser = require("body-parser");
-require('dotenv').config();
+// const express = require("express");
+// var bodyParser = require("body-parser");
+// require('dotenv').config();
+
+// const port = 5000
+// const app = express();
+// const TaskRoutes = require('./src/components/tasks/TaskRoutes');
+// const setupDB = require("./src/config/Db");
+
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// setupDB()
+
+// app.use('/tasks', TaskRoutes)
+
+// app.listen(port, () => {
+//     console.log(`server running on port ${port}`);
+//   });
+const express = require('express')
+
+const app = express()
 
 const port = 5000
-const app = express();
-const TaskRoutes = require('./src/components/tasks/TaskRoutes');
-const setupDB = require("./src/config/Db");
 
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-setupDB()
-
-app.use('/tasks', TaskRoutes)
+app.get('/', (req, res)=>{
+    console.log('hellow world - route /');
+    res.send('hello world')
+    // everytime call when we invoke get method of server
+})
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-  });
+  console.log(`server running on port ${port}`)
+})
